a8d295e59dcd71174914c19ba7912de45de1bfe1
SOMERITO <somerssc02@gmail.com>
2026-09-16 11:26:46 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,62 @@
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open('tasky-v1').then((cache) => {
-        return cache.addAll(['./index.html', './manifest.json', './icon.png']);
-    }));
+const CACHE_NAME = 'tasky-v27';
+
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
 });
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key.startsWith('tasky-') && key !== CACHE_NAME)
+            .map((key) => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+
+  if (request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(request).then((cachedResponse) => {
+      if (cachedResponse) return cachedResponse;
+
+      return fetch(request)
+        .then((networkResponse) => {
+          if (
+            !networkResponse ||
+            networkResponse.status !== 200 ||
+            networkResponse.type === 'opaque'
+          ) {
+            return networkResponse;
+          }
+
+          const copy = networkResponse.clone();
+
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(request, copy);
+          });
+
+          return networkResponse;
+        })
+        .catch(() => caches.match('./index.html'));
+    })
+  );
 });
